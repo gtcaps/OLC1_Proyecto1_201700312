a861bbc9419c90f1736c9ebc612d0c9932dc874a
Aybson Mercado <aybsonmg@gmail.com>
2020-08-25 22:53:57 -0600

    Agregando enum con los tipos de tokens del analizador de js

---
+++ AnalizadorLexicoJS/TipoToken.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+class TipoToken(Enum):
+    IDENTIFICADOR = 1
+    CADENA = 2
+    CARACTER = 3
+    IGUAL = 4
+    POR = 5
+    PUNTO_Y_COMA = 6
+    PUNTO = 7
+    DIVISION = 8
+    MAS = 9
+    MENOS = 10
+    PARENTESIS_IZQ = 11
+    PARENTESIS_DER = 12
+    LLAVE_IZQ = 13
+    LLAVE_DER = 14
+    EXCLAMACION = 15
+    MENOR = 16
+    MAYOR = 17
+    AND = 18
+    OR = 19 
+    PALABRA_RESERVADA = 20
